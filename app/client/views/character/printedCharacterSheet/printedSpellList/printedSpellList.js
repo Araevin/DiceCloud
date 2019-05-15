66e93ee3653f7080319df04ffad4adeaade0511d
Araevin <6397700+Araevin@users.noreply.github.com>
2019-05-15 01:29:51 -0400

    First layout of printed spell list format.

--- app/client/views/character/printedCharacterSheet/printedSpellList/printedSpellList.js
+++ app/client/views/character/printedCharacterSheet/printedSpellList/printedSpellList.js
@@ -17,4 +17,38 @@ Template.printedSpellList.helpers({
     evaluateDescription: function (charId, spellList) {
         return evaluateString(charId, spellList.description);
     },
+    columnList: function () {
+        return ["1", "2", "3"];
+    },
+    rowList: function () {
+        return [1, 2, 3, 4];
+    },
+    spellGroupColumns: function (charId, spellList, col, row) {
+        let spellsOfLevelCount = function (charId, spellList, spellLevel) {
+            return Spells.find({
+                charId: charId,
+                "parent.id": spellList._id, level: spellLevel
+            }).count();
+        };
+        let spellLevels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let columnList = {
+            "1": [],
+            "2": [],
+            "3": [],
+        };
+        var counter = 1;
+        for (var i = 0; i < spellLevels.length; i += 1) {
+            if (spellsOfLevelCount(charId, spellList, spellLevels[i]) > 0) {
+                columnList[parseInt(((counter - 1) % 3) + 1)].push(spellLevels[i]);
+                counter++;
+            }
+        };
+        return columnList[col][row - 1];
+    },
+    itemExists: function (item) {
+        if (item != 0 && (!item || item == null || item == undefined)) {
+            return false;
+        }
+        return true;
+    },
 });
